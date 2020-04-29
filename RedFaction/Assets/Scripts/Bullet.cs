57ed0815bcdf6152f91e27b5f17b0e9078d1d9d8
BEN OTHMAN SAMY p1707136 <samy.ben-othman@etu.univ-lyon1.fr>
2020-04-29 16:06:26 +0200

    Ajout d'un personnage qui peut tirer (balles affectées par la gravité + mode full auto ou mode coup par coup)

---
+++ RedFaction/Assets/Scripts/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody bulletCasing;
+    private Rigidbody bullet;
+    public int ejectSpeed = 100;
+    public float fireRate = 0.5f;
+    private float fireCoolDown = 0f;
+    private bool fullAuto = false;
+
+    void Update()
+    {
+        
+        //tirer sans coup par coup
+        if(Input.GetButton("Fire1") && Time.time> fireCoolDown)
+        {
+            fireCoolDown = Time.time + fireRate;
+            bullet = Instantiate(bulletCasing, transform.position, transform.rotation);
+            bullet.velocity = transform.TransformDirection(Vector3.left * ejectSpeed);
+        }
+
+        if (Input.GetKeyDown("v"))
+        {
+            fullAuto = !fullAuto;
+        }
+
+        if(fullAuto == true)
+        {
+            fireRate = 0.1f;
+        }
+        else
+        {
+            fireRate = 0.5f;
+        }
+    }
+
+}
