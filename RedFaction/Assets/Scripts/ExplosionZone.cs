d0efab15b8c3449eacad8c27346920f29e4c3c99
BEN OTHMAN SAMY p1707136 <samy.ben-othman@etu.univ-lyon1.fr>
2020-04-30 16:23:14 +0200

    Ajout d'une animation pour les grenades (animation d'explosions) + la grenade fait des dégats de zone

---
+++ RedFaction/Assets/Scripts/ExplosionZone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionZone : MonoBehaviour
+{
+    public float timer;
+    void Start()
+    {
+        StartCoroutine(soundExplode());
+    }
+    
+    public IEnumerator soundExplode()
+    {
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(timer);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider hit)
+    {
+        if(hit.gameObject.tag == "Ennemi")
+        {
+            hit.gameObject.GetComponent<EnnemieHealth>().ennemieHealth -= 50;
+        }
+    }
+}
