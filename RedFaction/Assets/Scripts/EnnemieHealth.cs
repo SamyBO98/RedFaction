79adbb514cffc88788a4a22586a805592f433595
BEN OTHMAN SAMY p1707136 <samy.ben-othman@etu.univ-lyon1.fr>
2020-05-01 20:52:22 +0200

    Ajout d'un rand pour que lorsque l'ennemi meurt la boite de munition a une chance sur deux d'apparaitre

--- RedFaction/Assets/Scripts/EnnemieHealth.cs
+++ RedFaction/Assets/Scripts/EnnemieHealth.cs
@@ -7,13 +7,31 @@ public class EnnemieHealth : MonoBehaviour
 
     public int ennemieHealth;
     public int damage;
+    public GameObject ammoBox;
+    public int randAmmoAppear;
 
+    private void Start()
+    {
+        ammoBox.SetActive(false);
+    }
     void Update()
     {
         //Debug.Log(ennemieHealth);
         if(ennemieHealth <= 0)
         {
+            Random.InitState((int)System.DateTime.Now.Ticks);
+            randAmmoAppear = Random.Range(0, 2);
+            Debug.Log(randAmmoAppear);
             Destroy(gameObject);
+            if(randAmmoAppear == 1)
+            {
+                ammoBox.SetActive(true);
+            }
+            else
+            {
+                ammoBox.SetActive(false);
+            }
+            
         }
     }
 
