3aaa3225a12f01efd0624646a70258c7c778b543
BEN OTHMAN SAMY p1707136 <samy.ben-othman@etu.univ-lyon1.fr>
2020-05-01 20:06:05 +0200

    Ajout de boite de munitions dans la map pour pouvoir recharger ses munitions si on les a toutes utilisées

---
+++ RedFaction/Assets/Scripts/AmoBox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmoBox : MonoBehaviour
+{
+    private Bullet b1;
+    private Bullet b2;
+    public int addReserveM4A1;
+    public int addReservePistol;
+    public AudioClip pickUpAmmo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        b1 = GameObject.Find("Eject").GetComponent<Bullet>();
+        b2 = GameObject.Find("Eject2").GetComponent<Bullet>();
+    }
+
+    private void OnTriggerEnter(Collider hit)
+    {
+        if(hit.gameObject.tag == "Player")
+        {
+            b1.reserve += addReserveM4A1;
+            b2.reserve += addReservePistol;
+            GetComponent<AudioSource>().PlayOneShot(pickUpAmmo);
+            StartCoroutine(DestroyAmmoBox());
+        }
+    }
+
+    public IEnumerator DestroyAmmoBox()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+
+}
