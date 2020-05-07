b026a46fcd783200822854f849e21c0c5095a423
BEN OTHMAN SAMY p1707136 <samy.ben-othman@etu.univ-lyon1.fr>
2020-05-07 02:13:11 +0200

    Ajout des Assets

--- RedFaction/Assets/Scripts/WeaponInventory.cs
+++ RedFaction/Assets/Scripts/WeaponInventory.cs
@@ -6,12 +6,16 @@ public class WeaponInventory : MonoBehaviour
 {
     public GameObject childObject;
     public Transform parentObject;
+    public float x;
+    public float y;
+    public float z;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
             childObject.transform.SetParent(parentObject);
+            childObject.transform.localRotation = Quaternion.Euler(x, y, z);
         }
     }
 }
