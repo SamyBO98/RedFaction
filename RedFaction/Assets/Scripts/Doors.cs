0af17b3423b70d2418cd3ff52812cf7f3797677e
BEN OTHMAN SAMY p1707136 <samy.ben-othman@etu.univ-lyon1.fr>
2020-05-09 18:40:11 +0200

    Ajout de portes coulissantes qui s'ouvrent quand le joueur s'approche

---
+++ RedFaction/Assets/Scripts/Doors.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    public GameObject doorLeft;
+    public GameObject doorRight;
+    public Transform target;
+    public Transform target2;
+    Vector3 original;
+    Vector3 original2;
+    public bool open;
+    public int speed;
+
+    private void Start()
+    {
+        original = doorLeft.transform.position;
+        original2 = doorRight.transform.position;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            open = true;
+            //Debug.Log("ouverture");
+            Open();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            open = true;
+            //Debug.Log("ouverture");
+            Open();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+        doorLeft.transform.position = original;
+        doorRight.transform.position = original2;
+    }
+
+    void Open()
+    {
+        doorLeft.transform.position = Vector3.MoveTowards(doorLeft.transform.position, target.position, speed * Time.deltaTime);
+        doorRight.transform.position = Vector3.MoveTowards(doorRight.transform.position, target2.position, speed * Time.deltaTime);
+    }
+}
