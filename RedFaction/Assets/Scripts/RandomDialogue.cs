2fcf21f8307cd7b864400d29449862793ddcfdcb
BEN OTHMAN SAMY p1707136 <samy.ben-othman@etu.univ-lyon1.fr>
2020-05-12 21:45:06 +0200

    Ajout d'un dialogue entre nous et Eos (chef de notre groupe)

--- RedFaction/Assets/Scripts/RandomDialogue.cs
+++ RedFaction/Assets/Scripts/RandomDialogue.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RandomDialogue : MonoBehaviour
 {
@@ -8,6 +9,12 @@ public class RandomDialogue : MonoBehaviour
     public AudioClip audioSpeak;
     public Collider col;
     public AudioSource test;
+    public Canvas eosDialogue1;
+    public Canvas dialogue1;
+    public Canvas dialogue2;
+    public Canvas dialogue3;
+    public Canvas dialogue4;
+    public Canvas dialogue5;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
@@ -21,7 +28,51 @@ public class RandomDialogue : MonoBehaviour
     public IEnumerator SoundTime()
     {
         GetComponent<AudioSource>().PlayOneShot(audioSpeak);
-        yield return new WaitForSeconds(audioSpeak.length);
+        yield return new WaitForSeconds(21);
+        if(eosDialogue1 != null)
+        {
+            eosDialogue1.enabled = true;
+            
+
+        }
+        if(dialogue1 != null)
+        {
+            dialogue1.enabled = true;
+            yield return new WaitForSeconds(6.7f);
+            dialogue1.enabled = false;
+        }
+
+        if(dialogue2 != null)
+        {
+            dialogue2.enabled = true;
+            yield return new WaitForSeconds(3.5f);
+            dialogue2.enabled = false;
+        }
+
+        if(dialogue3 != null)
+        {
+            dialogue3.enabled = true;
+            yield return new WaitForSeconds(4f);
+            dialogue3.enabled = false;
+        }
+
+        if(dialogue4 != null)
+        {
+            dialogue4.enabled = true;
+            yield return new WaitForSeconds(6.2f);
+            dialogue4.enabled = false;
+        }
+        
+        if(dialogue5 != null)
+        {
+            dialogue5.enabled = true;
+            yield return new WaitForSeconds(2);
+            dialogue5.enabled = false;
+            eosDialogue1.enabled = false;
+        }
+        
+        
+        
         if(test != null)
         {
             test.Play();
