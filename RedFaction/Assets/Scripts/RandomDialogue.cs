ea66a4a413a59cfb9c9498fcf404cdb17d1546e8
BEN OTHMAN SAMY p1707136 <samy.ben-othman@etu.univ-lyon1.fr>
2020-05-12 19:44:18 +0200

    Ajout de la suite de la map + modification et ajouts de certains sons d'ambiance

---
+++ RedFaction/Assets/Scripts/RandomDialogue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDialogue : MonoBehaviour
+{
+   
+    public AudioClip audioSpeak;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            
+            StartCoroutine(SoundTime());
+        }
+    }
+
+
+    public IEnumerator SoundTime()
+    {
+        GetComponent<AudioSource>().PlayOneShot(audioSpeak);
+        yield return new WaitForSeconds(audioSpeak.length);
+        Destroy(gameObject);
+    }
+}
