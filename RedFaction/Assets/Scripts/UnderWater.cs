841c4ed9cbfe6952f9507c32b402fbcdde5b464f
BEN OTHMAN SAMY p1707136 <samy.ben-othman@etu.univ-lyon1.fr>
2020-05-15 17:05:02 +0200

    Ajout d'une animation quand le personnage est sous l'eau (l'écran devient bleu)

---
+++ RedFaction/Assets/Scripts/UnderWater.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderWater : MonoBehaviour
+{
+    public GameObject water;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Throw")
+        {
+            water.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        water.SetActive(false);
+    }
+}
